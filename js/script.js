9575af247c7b5333b645cbae165f907e36786868
Adrian Litewski <litewskidev@gmail.com>
2022-11-07 23:16:47 +0100

    FIX: randomNumber var & playerInput var

--- js/script.js
+++ js/script.js
@@ -3,39 +3,23 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'rock';
-} else {
-  computerMove = 'unknown movement';
-}
-printMessage('My move: ' + computerMove);
-if (randomNumber == '2') {
+} else if (randomNumber == '2') {
   computerMove = 'paper';
-} else {
-  computerMove = 'unknown movement';
-}
-printMessage('My move: ' + computerMove);
-if (randomNumber == '3') {
+} else if (randomNumber == '3') {
   computerMove = 'scissors';
 } else {
-  computerMove = 'unknown movement';
+  computerMove = 'nieznany ruch';
 }
 printMessage('My move: ' + computerMove);
 
 var playerMove, playerInput;
-playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
-console.log('The typed answer is: ' + playerInput);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
   playerMove = 'rock';
-} else {
-  playerMove = 'unknown movement';
-}
-printMessage('Your move: ' + playerMove);
-if (playerInput == '2') {
+} else if (playerInput == '2') {
   playerMove = 'paper';
-} else {
-  playerMove = 'unknown movement';
-}
-printMessage('Your move: ' + playerMove);
-if (playerInput == '3') {
+} else if (playerInput == '3') {
   playerMove = 'scissors';
 } else {
   playerMove = 'unknown movement';
