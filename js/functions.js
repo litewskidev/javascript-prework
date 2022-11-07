28f8279a1565a7100ad8014e1379a23f028ca8a0
Adrian Litewski <litewskidev@gmail.com>
2022-11-07 21:38:28 +0100

    add variables: computerMove & playerMove

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, playerMove;
+computerMove = 'rock';
+printMessage('I played ' + computerMove + '! If your move is paper, you win!');
+playerMove = 'paper';
+printMessage('I played ' + playerMove + '! If your move is scissors, you win!');
