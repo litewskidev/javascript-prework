e1c37453118444ad42d91111c5c35213271831b9
Adrian Litewski <litewskidev@gmail.com>
2022-11-07 22:13:58 +0100

    add randomNumber var for computerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-var computerMove, playerMove;
-computerMove = 'rock';
-printMessage('I played ' + computerMove + '! If your move is paper, you win!');
-playerMove = 'paper';
-printMessage('I played ' + playerMove + '! If your move is scissors, you win!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'rock';
+} else {
+  computerMove = 'unknown movement';
+}
+printMessage('My move: ' + computerMove);
+if (randomNumber == '2') {
+  computerMove = 'paper';
+} else {
+  computerMove = 'unknown movement';
+}
+printMessage('My move: ' + computerMove);
+if (randomNumber == '3') {
+  computerMove = 'scissors';
+} else {
+  computerMove = 'unknown movement';
+}
+printMessage('My move: ' + computerMove);
