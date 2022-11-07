1462f77c84da9222a5c908f7539f64e42c121767
Adrian Litewski <litewskidev@gmail.com>
2022-11-07 22:25:21 +0100

    add playerInput var for playerMove

--- js/script.js
+++ js/script.js
@@ -19,3 +19,25 @@ if (randomNumber == '3') {
   computerMove = 'unknown movement';
 }
 printMessage('My move: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+console.log('The typed answer is: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'rock';
+} else {
+  playerMove = 'unknown movement';
+}
+printMessage('Your move: ' + playerMove);
+if (playerInput == '2') {
+  playerMove = 'paper';
+} else {
+  playerMove = 'unknown movement';
+}
+printMessage('Your move: ' + playerMove);
+if (playerInput == '3') {
+  playerMove = 'scissors';
+} else {
+  playerMove = 'unknown movement';
+}
+printMessage('Your move: ' + playerMove);
