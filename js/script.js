61141d8240b0438b02c17d1da4fa3c6c09e6f54c
Adrian Litewski <litewskidev@gmail.com>
2022-11-07 22:02:36 +0100

    remove vars from functions.js and add to script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'rock';
+printMessage('I played ' + computerMove + '! If your move is paper, you win!');
+playerMove = 'paper';
+printMessage('I played ' + playerMove + '! If your move is scissors, you win!');
